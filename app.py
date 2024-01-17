bca1ceb744ab0d0a1b61c3d0c71f4fb962b8ce0f
jacobm3642 <jacobmiller0@protonmail.ch>
2024-01-17 23:42:07 +1300

    db handlers added

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import flask
 import sqlite3
+from handers import db_tools
+
+#db_handler = db_tools.db_handler()
+#users = db_handler.add_db("Users", "users.db")
+#db_handler.add_query("Users", "get_all_users", "SELECT * FROM users", read_only=True)
+#db_handler.databases["Users"].set_perms(0, 99, 99)
+#print(db_handler.execute_premade_query("Users","get_all_users",0))
+
 
 app = flask.Flask(__name__, static_url_path='/static')
 
