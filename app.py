9b6b63089600065eda12d8bd79c5e1a2ca528b40
jacobm3642 <jacobmiller0@protonmail.ch>
2024-02-13 21:41:11 +1300

    data base setup

--- app.py
+++ app.py
@@ -6,25 +6,7 @@ from flask_cors import CORS
 
 db_handler = db_tools.db_handler()
 users = db_handler.add_db("Users", "users.db")
-db_handler.add_query("Users", "register_user", "INSERT INTO Users (UserName, Password, Email) VALUES (?, ?, ?);")
-db_handler.add_query("Users", "get_all_users", "SELECT * FROM users", read_only=True)
-db_handler.add_query("Users", "remove_all_users", "DELETE FROM Users;")
-db_handler.add_query("Users", "create_token_table", "CREATE TABLE token_table(userID INTEGER, token varchar(255), FOREIGN KEY(userID) REFERENCES users(Id))")
-db_handler.add_query("Users", "display_tables", "SELECT name FROM sqlite_master WHERE type='table'")
-db_handler.add_query("Users", "add_token", "INSERT INTO your_table_name (userID, token) VALUES (?, ?)")
-db_handler.add_query("Users", "get_user_id", "SELECT Id FROM Users WHERE UserName = ?", read_only=True)
 db_handler.databases["Users"].set_perms(0, 2, 99)
-# print(db_handler.execute_premade_query("Users","get_all_users",0))
-# db_handler.execute_premade_query("Users","create_token_table", 2)
-print(db_handler.execute_premade_query("Users", "display_tables", 2))
-
-def insert_token(userId):
-    token = token_urlsafe(32)
-    try:
-        db_handler.execute_premade_query("Users", "add_token", 2, parameters=(userId, token))
-    except:
-        return insert_token(userId)
-
 
 app = flask.Flask(__name__, static_url_path='/static')
 CORS(app)
@@ -46,21 +28,30 @@ def index():
 def login():
     pass
 
-def data_stringify(string):
-    string = str(string)
-    if string.legnth()>=255:
-        raise ValueError("TO LONG")
-    return string
-
 @app.route("/register", methods=['POST'])
 def register():
-    data = [str(i) for i in flask.request.get_json()]
-    username, password, email = data
-    db_handler.execute_premade_query("Users", "register_user", permission_level=2, parameters=(username, password, email))
-    out_db()
-    id = db_handler.execute_premade_query("Users", "get_user_id", 0, parameters=(username))
-    print(id)
-    return flask.jsonify([""]), 200
+    try:
+        data = [data_stringify(i) for i in flask.request.get_json()]
+        username, password, email = data
+        db_handler.execute_premade_query("Users", "register_user", permission_level=2, parameters=(username, password, email))
+        out_db()
+        id = db_handler.execute_premade_query("Users", "get_user_id", 0, parameters=(username))
+        token = insert_token(id[0][0])
+        return flask.jsonify([token]), 200
+    except:
+        return  flask.jsonify(["fuck"]), 500
+
+
+@app.route("/test", methods=['POST'])
+def test():
+    data = flask.request.get_json()
+    return flask.jsonify(data)
+
+def data_stringify(string):
+    string = str(string)
+    if len(string) >= 255:
+        raise ValueError("TO LONG")
+    return string
 
 def out_db():
     print(db_handler.execute_premade_query("Users","get_all_users",0))
@@ -68,11 +59,42 @@ def out_db():
 def clear():
     db_handler.execute_premade_query("Users", "remove_all_users", 2)
 
-@app.route("/test", methods=['POST'])
-def test():
-    data = flask.request.get_json()
-    return flask.jsonify(data)
+def insert_token(userId):
+    token = token_urlsafe(32)
+    try:
+        if db_handler.execute_premade_query("Users", "add_token", 2, parameters=(userId, token)) != False:
+            return token
+        raise ValueError("token already exsiests")
+    except:
+        return insert_token(userId)
+
+def init_db_handler():
+    db_handler.add_query("Users", "create_token_table", "CREATE TABLE token_table(userID INTEGER, token varchar(255), FOREIGN KEY(userID) REFERENCES users(Id))")
+    db_handler.add_query("Users", "create_user_table", "create table users( Id INTEGER PRIMARY KEY AUTOINCREMENT, UserName varchar(255) NOT NULL UNIQUE, Password varchar(255) NOT NULL, Email varchar(255) NOT NULL UNIQUE);")
+
+
+    db_handler.add_query("Users", "register_user", "INSERT INTO Users (UserName, Password, Email) VALUES (?, ?, ?);")
+    db_handler.add_query("Users", "get_all_users", "SELECT * FROM users", read_only=True)
+    db_handler.add_query("Users", "remove_all_users", "DELETE FROM Users;")
+    db_handler.add_query("Users", "display_tables", "SELECT name FROM sqlite_master WHERE type='table'")
+    db_handler.add_query("Users", "add_token", "INSERT INTO token_table(userID, token) VALUES (?, ?)")
+    db_handler.add_query("Users", "get_user_id", "SELECT Id FROM Users WHERE UserName = ?", read_only=True)
+    db_handler.add_query("Users", "drop_table_user", "DROP TABLE users;")
+    db_handler.add_query("Users", "drop_table_token", "DROP TABLE token_table;")
+
+def runtime_test():
+    print(db_handler.execute_premade_query("Users","get_all_users",0))
+    # db_handler.execute_premade_query("Users","create_token_table", 2)
+    print(db_handler.execute_premade_query("Users", "display_tables", 2))
+
+def reset_db():
+    db_handler.execute_premade_query("Users","drop_table_user", 99)
+    db_handler.execute_premade_query("Users","drop_table_token", 99)
+    db_handler.execute_premade_query("Users","create_user_table", 99)
+    db_handler.execute_premade_query("Users","create_token_table", 99)
+
 
 if __name__ == "__main__":
-    clear()
+    init_db_handler()
+    reset_db()
     app.run(host="127.0.0.1", port=5000)
